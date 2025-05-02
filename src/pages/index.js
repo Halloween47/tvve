2df85ca852c6f77c895294dd2b2c4b4e2c8d91aa
TomCat <lecontethomas.contact@proton.me>
2025-05-02 22:38:02 +0200

    PAGE TEST - test prise en compte next

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+// pages/index.js
+import VideoPlayer from '../components/VideoPlayer';
+import TextOverlay from '../components/TextOverlay';
+
+const Home = () => {
+  const videoSrc = '/videos/sample-video.mp4';
+  const texts = [
+    { content: 'Hello World!', start: 2, end: 5, top: '10px', left: '10px', color: 'white', fontSize: '24px' },
+    { content: 'Next.js is awesome!', start: 6, end: 9, top: '50px', left: '50px', color: 'red', fontSize: '20px' },
+  ];
+
+  return (
+    <div>
+      <h1>Video Text Overlay</h1>
+      <VideoPlayer src={videoSrc}>
+        <TextOverlay texts={texts} />
+      </VideoPlayer>
+    </div>
+  );
+};
+
+export default Home;
